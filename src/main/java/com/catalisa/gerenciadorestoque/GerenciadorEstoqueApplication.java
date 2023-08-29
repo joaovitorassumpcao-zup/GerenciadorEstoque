060fe28acb4774a04d38fe5c9835a57de41dfb83
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-29 20:32:43 -0300

    Initial

---
+++ src/main/java/com/catalisa/gerenciadorestoque/GerenciadorEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.gerenciadorestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEstoqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorEstoqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catalisa/gerenciadorestoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.gerenciadorestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
