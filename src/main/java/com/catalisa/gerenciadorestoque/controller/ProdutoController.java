4b98550a97e6578c49c6d3bc1f660c1216eade74
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-31 20:34:53 -0300

    ProdutoController atualizado.

--- src/main/java/com/catalisa/gerenciadorestoque/controller/ProdutoController.java
+++ src/main/java/com/catalisa/gerenciadorestoque/controller/ProdutoController.java
@@ -1,8 +1,12 @@
 package com.catalisa.gerenciadorestoque.controller;
 
+import com.catalisa.gerenciadorestoque.dto.ProdutoDTO;
 import com.catalisa.gerenciadorestoque.service.ProdutoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -14,5 +18,39 @@ public class ProdutoController {
         this.produtoService = produtoService;
     }
 
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> criarProduto(@RequestBody ProdutoDTO produtoDTO) {
+        ProdutoDTO novoProduto = produtoService.create(produtoDTO);
+        return new ResponseEntity<>(novoProduto, HttpStatus.CREATED);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> listarProdutos() {
+        List<ProdutoDTO> produtos = produtoService.findAll();
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> obterProdutoPorId(@PathVariable Long id) {
+        return produtoService.findByID(id)
+                .map(produto -> new ResponseEntity<>(produto, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+        return produtoService.update(id, produtoDTO)
+                .map(produtoAtualizado -> new ResponseEntity<>(produtoAtualizado, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirProduto(@PathVariable Long id) {
+        try {
+            produtoService.delete(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
