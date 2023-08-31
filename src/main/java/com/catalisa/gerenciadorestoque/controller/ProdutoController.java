d3a06e9110654edad4db6947b6fc23988ef951d4
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-31 20:14:10 -0300

    ProdutoController.java

---
+++ src/main/java/com/catalisa/gerenciadorestoque/controller/ProdutoController.java
@@ -0,0 +1,18 @@
+package com.catalisa.gerenciadorestoque.controller;
+
+import com.catalisa.gerenciadorestoque.service.ProdutoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+
+}
